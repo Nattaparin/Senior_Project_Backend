54b12cd077fbbdf09433d013016f36c8e0477845
Nattaparin <nattaparin455@gmail.com>
2023-10-02 18:13:57 +0700

    Update from OpenAI API

--- controller/chat.py
+++ controller/chat.py
@@ -14,13 +14,28 @@ import json
 import openai
 # DISTANCE_LAST = None  # ระยะทางล่าสุด
 # DISTANCE_NOW = None  # ระยะทางตอนนี้
+import git
+
+# กำหนด repository ที่ต้องการทำงาน
+repo_path = "C:/Users/ASUS TUF FA506/OneDrive/เดสก์ท็อป/project/Senior_Project_Backend"
+repo = git.Repo(repo_path)
+
+# เพิ่มไฟล์ที่มีการเปลี่ยนแปลง
+repo.git.add(update=True)
+
+# Commit การเปลี่ยนแปลง
+repo.git.commit("-m", "Update from OpenAI API")
+
+# Push การเปลี่ยนแปลงไปยัง remote
+origin = repo.remote(name='origin')
+origin.push()
 
 # Load the car issues dictionary from the JSON file
 with open('C:/Users/ASUS TUF FA506/OneDrive/เดสก์ท็อป/project/Senior_Project_Backend/controller/car_issues.json', 'r', encoding='utf-8') as f:
     car_issues_loaded = json.load(f)
 
 # Set up the OpenAI API key
-openai.api_key = "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"
+openai.api_key = "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"
 
 def remove_stopwords(text):
     tokens = word_tokenize(text, engine="newmm")
